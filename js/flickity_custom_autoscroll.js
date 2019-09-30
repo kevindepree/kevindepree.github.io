b9aa5378cdc6377bfa42ab84dc16f3e1874bbe44
Nick Lill <mail.nicklill@gmail.com>
2019-09-30 12:03:31 -0400

    Update flickity_custom_autoscroll.js
    Added desandro's fix for cell drift

--- js/flickity_custom_autoscroll.js
+++ js/flickity_custom_autoscroll.js
@@ -1,3 +1,38 @@
+//   Fix for mobile slide stutter(snapping to cell start/end)
+
+var transformProperty = ( function () {
+  var style = document.documentElement.style;
+  if ( typeof style.transform == 'string' ) {
+    return 'transform';
+  }
+  return 'WebkitTransform';
+})();
+
+Flickity.prototype.positionSlider = function() {
+  var x = this.x;
+  // wrap position around
+  if ( this.options.wrapAround && this.cells.length > 1 ) {
+    x = utils.modulo( x, this.slideableWidth );
+    x = x - this.slideableWidth;
+    this.shiftWrapCells( x );
+  }
+
+  x = x + this.cursorPosition;
+  // reverse if right-to-left and using transform
+  x = this.options.rightToLeft && transformProperty ? -x : x;
+  var value = this.getPositionValue( x );
+  // only use 2d rendering
+  this.slider.style[ transformProperty ] = 'translateX(' + value + ')';
+
+  // scroll event
+  var firstSlide = this.slides[0];
+  if ( firstSlide ) {
+    var positionX = -this.x - firstSlide.target;
+    var progress = positionX / this.slidesWidth;
+    this.dispatchEvent( 'scroll', null, [ progress, positionX ] );
+  }
+};
+
 //   Variables
 
 let tickerSpeed = .2;
